97a6d3bf08a70083814ea8ef562a3c7f2d6c0b92
abhi-ak-18 <monuabhishek.232@gmail.com>
2022-04-08 16:34:17 +0530

    hasrouter

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import TextForm from './components/TextForm';
 
 import React from "react";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
