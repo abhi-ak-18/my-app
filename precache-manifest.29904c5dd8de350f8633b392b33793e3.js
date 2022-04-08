7373af3030de2b772ff16dd5887f5d1ac156879a
abhi-ak-18 <monuabhishek.232@gmail.com>
2022-04-08 16:35:58 +0530

    Updates

---
+++ precache-manifest.29904c5dd8de350f8633b392b33793e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f31ac158cecc683cd00046c65436f6c",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "1f02173752f404ea7feb",
+    "url": "/my-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "63d9722c3b8f25482be9",
+    "url": "/my-app/static/js/2.969fda87.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/my-app/static/js/2.969fda87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a44adf130662295ebd4",
+    "url": "/my-app/static/js/3.93329e08.chunk.js"
+  },
+  {
+    "revision": "1f02173752f404ea7feb",
+    "url": "/my-app/static/js/main.f3268d50.chunk.js"
+  },
+  {
+    "revision": "a7dc055837f50925897f",
+    "url": "/my-app/static/js/runtime-main.c63f0fd7.js"
+  }
+]);
